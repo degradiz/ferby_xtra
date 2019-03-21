938e59bc749505f2c13ebe03c5cc696db9ec4f3e
degradiz <djgradiz@gmail.com>
2019-03-21 13:25:35 -0600

    WS Ferby para Xtra
    WS creado para ser consumido por Xtra cuando se canjea un cupón

---
+++ cuponused.php
@@ -0,0 +1,45 @@
+<?php
+if(isset($_GET['cupon_code'])){
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = 'ferby';
+$con = mysqli_connect($host, $user, $password, $db);
+
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} 
+$codigo = $_GET['cupon_code'];
+$status = "No";
+
+$sql = "SELECT id_generated, cupon_code, state FROM cupon_generated WHERE cupon_code='$codigo' ";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    	if ($row["state"]==1){$status = "Si";}
+        echo "id: " . $row["id_generated"]. " - cupon_code: " . $row["cupon_code"]. " - Consumido: " . $status . "<br>";
+    }
+
+    $sqlu = "UPDATE cupon_generated SET state=1 WHERE cupon_code='$codigo'";
+
+    if($row["state"]==1){
+		if ($con->query($sqlu) === TRUE) {
+		    echo "Consumo de cupón registrado.";
+		} else {
+		    echo "Error actualizando: " . $con->error;
+		}
+	}else{
+		echo "Cupon ya consumido.";
+	}
+} else {
+    echo "0 resultados del código de cupon enviado.";
+}
+
+
+
+$con->close();
+}
+?>
--- testws.php
+++ testws.php
@@ -1,7 +1,7 @@
 <?php 
 require_once 'AppEmulator/nusoap/lib/nusoap.php';
 
-$wsdl="http://localhost:53168/operaciones.asmx?WSDL";
+$wsdl="http://192.168.0.10:53168/operaciones.asmx?WSDL";
 
 $client=new nusoap_client($wsdl,true);
 $cupon_code = "de001";
@@ -9,14 +9,14 @@ $cupon_name = "Prueba Dennis";
 $cupon_description = "35";
 $cant = "10";
 
-$result=$client->call('Insert',array('cupon_code'=>$cupon_code,'nombre'=>$cupon_name,'descuento'=>$cupon_description,'cantidad'=>$cant));
+//$result=$client->call('Insert',array('cupon_code'=>$cupon_code,'nombre'=>$cupon_name,'descuento'=>$cupon_description,'cantidad'=>$cant));
 //$result=$client->call('Insert',array('cupon_code'=>'3ac-9','nombre'=>'dia del padre','descuento'=>'10','cantidad'=>'20'));
 
-//$result=$client->call('Exist',array('cupon_code'=>'a001'));
+$result=$client->call('sumar',array('x'=>'7','y'=>'1'));
 
 var_dump($result);
 
-echo $result['InsertResult'];
+echo $result['sumarResult'];
 
 
 
