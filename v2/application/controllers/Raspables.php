617b8cec42270cf65160d6ec4568813a41d7097a
degradiz <djgradiz@gmail.com>
2019-10-15 12:28:48 -0600

    Update Raspables.php

--- v2/application/controllers/Raspables.php
+++ v2/application/controllers/Raspables.php
@@ -130,7 +130,7 @@ public function generated(){
 
 			$actual_link = $_SERVER['REQUEST_URI'];
 			$array = explode("/", $actual_link);
-			$id= $array[count($array)-2];
+			$id= $array[count($array)-1];
 
 			$crud->where(array('scratch_generated.scratch_id'=> $id,'reclamado'=> 0	,'client_id' => null));
 
@@ -171,7 +171,7 @@ public function reclamados(){
 
 			$actual_link = $_SERVER['REQUEST_URI'];
 			$array = explode("/", $actual_link);
-			$id= $array[5];
+			$id= $array[count($array)-1];
 
 			$crud->where(array('scratch_generated.scratch_id'=> $id,'scratch_generated.reclamado'=> 1	));
 
@@ -212,7 +212,7 @@ public function asignados(){
 
 			$actual_link = $_SERVER['REQUEST_URI'];
 			$array = explode("/", $actual_link);
-			$id= $array[5];
+			$id= $array[count($array)-1];
 
 			$crud->where(array('scratch_generated.scratch_id'=> $id,'scratch_generated.client_id >'=> 0 ,'scratch_generated.reclamado' => 0	));
 
