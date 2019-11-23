8d88c9c7ef41f28e13eb442e5181e3f6c9345019
degradiz <djgradiz@gmail.com>
2019-11-22 18:50:09 -0600

    validación de que mande numero de factura

--- ws/wsMain.php
+++ ws/wsMain.php
@@ -511,9 +511,13 @@ switch ($action) {
         $place_id = $_GET['place_id'];
         $amt = $_GET['amt'];
         $tienda = $_GET['tienda'];
-        insertPoints_manually($username,$factura,$place_id,$amt,$tienda);
-        assign_number_lottery_identidad($tienda,$username,$amt);
-        assign_scratch_identidad($tienda,$username,$amt);
+        if($factura > 0){
+            insertPoints_manually($username,$factura,$place_id,$amt,$tienda);        
+            assign_number_lottery_identidad($tienda,$username,$amt);
+            assign_scratch_identidad($tienda,$username,$amt);
+        }else{
+            echo -1;
+        }
         break;   
 
     case 'assign_number_lottery_identidad':
